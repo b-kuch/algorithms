dbe1d21c9552718bc2f9fa5aa7ea7d7caa2546a6
b-kuch <kucharczyk.bt@gmail.com>
2020-11-01 17:43:29 +0100

    Clean main.c

--- src/main.c
+++ src/main.c
@@ -6,50 +6,40 @@
 #include "heapsort_rec.h"
 #include "heapsort_iter.h"
 #include "util.h"
+#include "time_complexity.h"
 
-#define MLD 1000000000.0
-
-// ifndef to hide error from IDE
-#ifndef CLOCK_PROCESS_CPUTIME_ID
-#define CLOCK_PROCESS_CPUTIME_ID 1
-#endif
+double Fn(int n)
+{
+    return n*log2(n);
+}
 
 int main()
 {
-    struct timespec tp0, tp1;
-    double Tn,Fn;
-    int* heap;
+    double Tn;
+    int* arr;
     char fname[1000]="numbers/nums.txt";
     bool sorted = true;
     srand(time(NULL));
-    // for(int n=1024;n<=1024*pow(2,20)&&sorted;n*=2)
-    // {
-    //     save_random_list(n, fname);
-    //     load_number_file_to_array(&heap,fname);
+    for(int n=1024;n<=1024*pow(2,20)&&sorted;n*=2)
+    {
+        save_random_list(n, fname);
+        load_number_file_to_array(&arr,fname);
+        Tn = my_timeit(&heap_sort_iter, n, arr);
+        sorted = is_sorted(n, arr, &smaller);
+        free(arr);
+        print_complexity(n, Tn, &Fn);
+    }
 
-    //     clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&tp0);
-    //     heap_sort_iter(n, heap);
-    //     clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&tp1);
-
-    //     sorted = is_sorted(n, heap);
-    //     free(heap);
-
-    //     Fn = n*log2(n);
-        
-    //     Tn = (tp1.tv_sec+tp1.tv_nsec/MLD) - (tp0.tv_sec+tp0.tv_nsec/MLD);
-    //     printf("n: %5d\t czas: %4.3lf \twspolczynnik: %3.0lf\t Fn=%3.0lf \n", n,  Tn, Fn/Tn, Fn);
-    // }
-
-    int n = 1000;
-    save_random_list(n, fname);
-    load_number_file_to_array(&heap,fname);
-    // print_list(n, heap);
-    // print_heap(n, heap);
-    heap_sort_iter(n, heap);
-    printf("\nJeden jeśli lista posortowana nierosnąco: %d\n\n", is_sorted(n, heap, &smaller));
-    save_list_to_file(n, heap, fname);
-    // print_list(n, heap);
-    // print_heap(n, heap);
-    free(heap);
+    // int n = 1000;
+    // save_random_list(n, fname);
+    // load_number_file_to_array(&arr,fname);
+    // // print_list(n, arr);
+    // // print_heap(n, arr);
+    // heap_sort_iter(n, arr);
+    // printf("\nJeden jeśli lista posortowana nierosnąco: %d\n\n", is_sorted(n, arr, &smaller));
+    // save_list_to_file(n, arr, fname);
+    // // print_list(n, arr);
+    // // print_heap(n, arr);
+    // free(arr);
     return 0;
 }
