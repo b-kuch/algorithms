c267bc714b172d0c5113c06a70daff4636238a52
b-kuch <kucharczyk.bt@gmail.com>
2020-11-01 23:43:16 +0100

    Spacing

--- src/time_complexity.c
+++ src/time_complexity.c
@@ -8,8 +8,8 @@
 double my_timeit(void (*function_pointer)(int, int*), int array_size, int* array)
 {
     struct timespec tp0, tp1;
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&tp0);
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &tp0);
     (*function_pointer)(array_size, array);
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&tp1);
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &tp1);
     return (tp1.tv_sec+tp1.tv_nsec/MLD) - (tp0.tv_sec+tp0.tv_nsec/MLD);
 }
