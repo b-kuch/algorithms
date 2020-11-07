e8ceb3e0a039bcf5e759fc802f1786ea585af3c8
b-kuch <kucharczyk.bt@gmail.com>
2020-11-07 20:37:24 +0100

    Add quicksort

---
+++ include/quicksort.h
@@ -0,0 +1,35 @@
+#ifndef QUICKSORT_FUNCTIONS
+#define QUICKSORT_FUNCTIONS
+
+#include <stdbool.h>
+#include <stdlib.h>
+
+// #include "quicksort.h"
+#include "util.h"
+
+int rightmost_partition(
+    int p,
+    int r,
+    int *array,
+    bool (*compare_f)(int, int));
+
+int randomized_partition(
+    int p,
+    int r,
+    int *array,
+    bool (*compare_f)(int, int));
+
+int median_partition(
+    int p,
+    int r,
+    int *array,
+    bool (*compare_f)(int, int));
+
+void my_quicksort(
+    int p,
+    int r,
+    int *array,
+    bool (*compare_f)(int, int),
+    int (*partition_method)(int, int, int *, bool (*)(int, int)));
+
+#endif
---
+++ src/quicksort.c
@@ -0,0 +1,77 @@
+#include "quicksort.h"
+
+int rightmost_partition(
+    int p,
+    int r,
+    int *array,
+    bool (*compare_f)(int, int))
+{
+    int div = array[r];
+    int q = p - 1;
+    for (int i = p; i <= r; i++)
+    {
+        if ((*compare_f)(array[i], div))
+        {
+            q++;
+            swap(&array[i], &array[q]);
+        }
+    }
+
+    if (q < r)
+    {
+        return q;
+    }
+    else
+    {
+        return q - 1;
+    }
+}
+
+int randomized_partition(
+    int p,
+    int r,
+    int *array,
+    bool (*compare_f)(int, int))
+{
+    srand(time(NULL));
+    int k = p + rand() % (r - p + 1);
+    swap(array + k, array + r);
+    return rightmost_partition(p, r, array, compare_f);
+}
+
+int median_partition(
+    int p,
+    int r,
+    int *array,
+    bool (*compare_f)(int, int))
+{
+    int m = (p + r) / 2;
+    if (array[m] < array[p])
+    {
+        swap(&array[p], &array[m]);
+    }
+    if (array[r] < array[p])
+    {
+        swap(&array[p], &array[r]);
+    }
+    if (array[m] < array[r])
+    {
+        swap(&array[m], &array[r]);
+    }
+    return rightmost_partition(p, r, array, compare_f);
+}
+
+void my_quicksort(
+    int p,
+    int r,
+    int *array,
+    bool (*compare_f)(int, int),
+    int (*partition_method)(int, int, int *, bool (*)(int, int)))
+{
+    if (r > p)
+    {
+        int pivot = (*partition_method)(p, r, array, compare_f);
+        my_quicksort(p, pivot, array, compare_f, partition_method);
+        my_quicksort(pivot + 1, r, array, compare_f, partition_method);
+    }
+}
