4774c89101ee9b4e658b50672c081da59b4e8650
b-kuch <kucharczyk.bt@gmail.com>
2020-11-07 15:18:30 +0100

    Add timeit for six argument quicksort

--- include/time_complexity.h
+++ include/time_complexity.h
@@ -2,8 +2,19 @@
 #define TIME_COMPLEXITY_FUNCTIONS
 
     #include <time.h>
-
+    #include <stdbool.h>
     #define MLD 1000000000.0
 
     double my_timeit(void (*function_pointer)(int, int*), int array_size, int* array);
+    double my_timeit6
+    (
+        void (*function_pointer)(int,  int,  int,  int *,
+            int (*)(int,  int,  int *), bool (*)(int,  int)),
+        int p,
+        int r,
+        int array_size,
+        int* array,
+        int (*divider)(int, int, int*), bool (*compare_f)(int, int)
+    );
+
 #endif
--- src/time_complexity.c
+++ src/time_complexity.c
@@ -13,3 +13,22 @@ double my_timeit(void (*function_pointer)(int, int*), int array_size, int* array
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &tp1);
     return (tp1.tv_sec+tp1.tv_nsec/MLD) - (tp0.tv_sec+tp0.tv_nsec/MLD);
 }
+
+double my_timeitQS
+    (
+        void (*function_pointer)(int,  int,  int,  int *, int (*)(int,  int,  int *), bool (*)(int,  int)),
+        int p,
+        int r,
+        int array_size,
+        int* array,
+        int (*divider)(int, int, int*),
+        bool (*compare_f)(int, int)
+    )
+{
+    struct timespec tp0, tp1;
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &tp0);
+    (*function_pointer)(p, r, array_size, array, divider, compare_f);
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &tp1);
+    
+    return (tp1.tv_sec+tp1.tv_nsec/MLD) - (tp0.tv_sec+tp0.tv_nsec/MLD);
+}
