0b0593e89a202c6aa8d83496e3788f77cf9aeb04
b-kuch <kucharczyk.bt@gmail.com>
2020-11-10 19:54:29 +0100

    insertsort

---
+++ include/insertsort.h
@@ -0,0 +1,7 @@
+#ifndef INSERTSORT
+#define INSERTSORT
+
+#include <stdbool.h>
+void insertsort(int n, int *array, bool(*compare_f)(int, int));
+
+#endif
---
+++ src/insertsort.c
@@ -0,0 +1,27 @@
+#include "insertsort.h"
+
+void insertsort(int size, int* array, bool(*compare_f)(int, int))
+{
+    for (int i=0;i<size;i++)
+    {
+        int l, r, key, m;
+			key = array[i];
+			l = 0;
+			r = i-1;
+			while ( l<=r )  //szukanie binarne miejsca w posortowanej liście
+			{
+				m=(l+r)/2;
+				if((*compare_f)(key, array[m]))
+				{
+					r = m-1;
+				} else {
+					l = m+1;
+				}
+			}
+			for(int j=i-1; j>=l; --j)   
+			{
+				array[j+1] = array[j];
+			}
+				array[l] = key;	// wstawienie elementu na miejsce w posortowanej liście
+    }
+}
