43f3b2c3ddbed4cefd2d03d3c45aad871058030f
b-kuch <kucharczyk.bt@gmail.com>
2020-11-17 20:48:12 +0100

    Format and spacing

--- src/file_io.c
+++ src/file_io.c
@@ -1,6 +1,6 @@
 #include "file_io.h"
 
-bool file_exists(char* fname)
+bool file_exists(char *fname)
 {
     FILE *f = fopen(fname, "r");
     if (f == NULL)
@@ -14,29 +14,29 @@ bool file_exists(char* fname)
         return true;
     }
 }
-int save_random_list(int n, char* fname)
+int save_random_list(int n, char *fname)
 {
     srand(time(NULL));
     FILE *f = fopen(fname, "w");
-    for (int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        fprintf(f,"%d\n", rand()%100 + 0);
+        fprintf(f, "%d\n", rand() % 100 + 0);
     }
-	fclose(f);
+    fclose(f);
     return n;
 }
 
-void save_list_to_file(int n, int* arr, char* fname)
+void save_list_to_file(int n, int *arr, char *fname)
 {
     FILE *f = fopen(fname, "w");
-    for (int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        fprintf(f,"%d\n", *(arr+i) );
+        fprintf(f, "%d\n", *(arr + i));
     }
-	fclose(f);
+    fclose(f);
 }
 
-int load_number_file_to_array(int** arr, char* fname)
+int load_number_file_to_array(int **arr, char *fname)
 {
     if (!file_exists(fname))
     {
@@ -44,21 +44,19 @@ int load_number_file_to_array(int** arr, char* fname)
         return 0;
     }
     int x;
-    int n=0;
+    int n = 0;
     FILE *f = fopen(fname, "r");
 
-    while (fscanf (f, "%d",&x) != EOF)
+    while (fscanf(f, "%d", &x) != EOF)
     {
         n++;
     }
     rewind(f);
 
+    *arr = (int *)malloc(n * sizeof(int));
 
-    *arr =(int*) malloc(n * sizeof(int));
-
-    
-    int i=0;
-    while (fscanf (f, "%d", &(*arr)[i]) != EOF)
+    int i = 0;
+    while (fscanf(f, "%d", &(*arr)[i]) != EOF)
     {
         i++;
     }
