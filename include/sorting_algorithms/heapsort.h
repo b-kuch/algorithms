32f1a872b0c67e519c7f6b9f57317549583d2fad
b-kuch <kucharczyk.bt@gmail.com>
2020-11-19 22:01:22 +0100

    Move heap-related functions from util; Format code

--- include/sorting_algorithms/heapsort.h
+++ include/sorting_algorithms/heapsort.h
@@ -1,16 +1,20 @@
 #ifndef HEAPSORT_FUNCTIONS
 #define HEAPSORT_FUNCTIONS
 
-    #include <stdbool.h>
+#include <stdbool.h>
 
-    #include "util.h"
+#include "util.h"
 
-    void heapify_iterative(int heap_size, int* heap, int parent);
-    void heapify_recursive(int heap_size, int* heap, int parent);
+void heapify_iterative(int heap_size, int *heap, int parent);
+void heapify_recursive(int heap_size, int *heap, int parent);
 
-    void build_heap(int heap_size, int* heap, void (*heapify)(int, int*, int));
-    void heap_sort(int heap_size, int* heap, void (*heapify)(int, int*, int));
-    void heap_sort_iterative(int heap_size, int* heap);
-    void heap_sort_recursive(int heap_size, int* heap);
+void build_heap(int heap_size, int *heap, void (*heapify)(int, int *, int));
+void heap_sort(int heap_size, int *heap, void (*heapify)(int, int *, int));
+void heap_sort_iterative(int heap_size, int *heap);
+void heap_sort_recursive(int heap_size, int *heap);
+
+#define MAX_PRINTABLE_HEAP 64
+void print_heap(int heap_size, int *heap);
+int heap_levels(int n);
 
 #endif
--- src/sorting_algorithms/heapsort.c
+++ src/sorting_algorithms/heapsort.c
@@ -1,14 +1,13 @@
 #include "heapsort.h"
 
-
-void heapify_iterative(int heap_size, int* heap, int parent)
+void heapify_iterative(int heap_size, int *heap, int parent)
 {
     int left, right;
     int larger = parent;
-    while(true)
+    while (true)
     {
-        left    = 2*parent+1;
-        right   = 2*parent+2;
+        left = 2 * parent + 1;
+        right = 2 * parent + 2;
         if (left < heap_size && heap[left] < heap[larger])
         {
             larger = left;
@@ -22,16 +21,16 @@ void heapify_iterative(int heap_size, int* heap, int parent)
             break;
         }
         swap(&heap[parent], &heap[larger]);
-        parent=larger;
+        parent = larger;
     }
 }
 
-void heapify_recursive(int heap_size, int* heap, int parent)
+void heapify_recursive(int heap_size, int *heap, int parent)
 {
     int left, right, larger;
     larger = parent;
-    left    = 2*parent+1;
-    right   = 2*parent+2;
+    left = 2 * parent + 1;
+    right = 2 * parent + 2;
     if (left < heap_size && heap[left] < heap[larger])
     {
         larger = left;
@@ -47,30 +46,57 @@ void heapify_recursive(int heap_size, int* heap, int parent)
     }
 }
 
-void build_heap(int heap_size, int* heap, void (*heapify)(int, int*, int))
+void build_heap(int heap_size, int *heap, void (*heapify)(int, int *, int))
 {
-    for(int i=heap_size/2-1;i>=0;i--)
+    for (int i = heap_size / 2 - 1; i >= 0; i--)
     {
         (*heapify)(heap_size, heap, i);
     }
 }
 
-void heap_sort(int heap_size, int* heap, void (*heapify)(int, int*, int))
+void heap_sort(int heap_size, int *heap, void (*heapify)(int, int *, int))
 {
     build_heap(heap_size, heap, heapify);
-    for(int i=heap_size-1;i>=0;i--)
+    for (int i = heap_size - 1; i >= 0; i--)
     {
         swap(&heap[0], &heap[i]);
         (*heapify)(i, heap, 0);
     }
 }
 
-void heap_sort_iterative(int heap_size, int* heap)
+void heap_sort_iterative(int heap_size, int *heap)
 {
     heap_sort(heap_size, heap, heapify_iterative);
 }
 
-void heap_sort_recursive(int heap_size, int* heap)
+void heap_sort_recursive(int heap_size, int *heap)
 {
     heap_sort(heap_size, heap, heapify_recursive);
+}
+
+void print_heap(int heap_size, int *heap)
+{
+    if (heap_size >= MAX_PRINTABLE_HEAP)
+    {
+        printf("n too big for console.\n");
+        return;
+    }
+    int levels = heap_levels(heap_size);
+    for (int i = 1; i < heap_size + 1; i *= 2)
+    {
+        spaces(pow(2, levels - 1) - 1);
+
+        for (int j = 0; j < i && i - 1 + j < heap_size; j++)
+        {
+            printf("%d", heap[i - 1 + j]);
+            spaces(pow(2, levels) - 1);
+        }
+        levels--;
+        printf("\n");
+    }
+}
+
+int heap_levels(int n)
+{
+    return ceil(log2(n));
 }
