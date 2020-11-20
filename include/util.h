b5908eeeaa766c43bb1918adc644a4eef9b3e6c5
b-kuch <kucharczyk.bt@gmail.com>
2020-11-20 01:13:32 +0100

    Repair inline functions; Format files

--- include/util.h
+++ include/util.h
@@ -1,5 +1,5 @@
-#ifndef UTIL_FUNCTIONS
-#define UTIL_FUNCTIONS
+#ifndef UTIL
+#define UTIL
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,14 +14,20 @@ void spaces(int amount);
 void print_array(int size, int *array);
 void print_complexity(int n, double time, double (*function_pointer)(int));
 
-inline bool greater(int a, int b);
-inline bool greater_equal(int a, int b);
-inline bool smaller(int a, int b);
-inline bool smaller_equal(int a, int b);
+inline bool greater(int a, int b) { return a > b; }
+inline bool greater_equal(int a, int b) { return a >= b; }
+inline bool smaller(int a, int b) { return a < b; }
+inline bool smaller_equal(int a, int b) { return a <= b; }
 
 bool is_sorted(int size, int *array, bool (*compare_function)(int, int));
-inline void swap(int *a, int *b);
-inline bool in_range(int x, int a, int b);
+inline void swap(int *a, int *b)
+{
+    int c = *a;
+    *a = *b;
+    *b = c;
+}
+// if a<=x<=b
+inline bool in_range(int x, int a, int b) { return x >= a && x <= b; }
 
 #define ASCII_DIGITS 48, 57
 #define ASCII_UPPERCASE 65, 90
--- src/util.c
+++ src/util.c
@@ -1,76 +1,61 @@
 #include "util.h"
 
-void create_array(int array_size, int** heap)
+void create_array(int array_size, int **heap)
 {
     (*heap) = malloc(sizeof(int) * array_size);
 
-    for(int i=0;i<array_size;i++)
+    for (int i = 0; i < array_size; i++)
     {
-        (*heap)[i] = rand()%10 + 0;
+        (*heap)[i] = rand() % 10 + 0;
     }
 }
 
-void create_array_x(int array_size, int** heap, int x)
+void create_array_x(int array_size, int **heap, int x)
 {
     (*heap) = malloc(sizeof(int) * array_size);
 
-    for(int i=0;i<array_size;i++)
+    for (int i = 0; i < array_size; i++)
     {
         (*heap)[i] = x;
     }
 }
 
-
 void spaces(int amount)
 {
-    for(int i=0; i<amount; i++)
+    for (int i = 0; i < amount; i++)
         printf(" ");
 }
 
-void print_array(int size, int* array)
+void print_array(int size, int *array)
 {
-    for(int* i=array;i<array+size;i++)
+    for (int *i = array; i < array + size; i++)
     {
-        printf("%d ",*i);
+        printf("%d ", *i);
     }
     printf("\n");
 }
 
-void print_complexity(int n, double time, double(*function_pointer)(int))
+void print_complexity(int n, double time, double (*function_pointer)(int))
 {
     double fn = (*function_pointer)(n);
     printf("n: %5d\t czas: %4.3lf \twspolczynnik: %3.0lf\t Fn=%3.0lf \n",
-                n,  time, fn/time, fn);
+           n, time, fn / time, fn);
 }
 
+extern bool greater(int a, int b);
+extern bool greater_equal(int a, int b);
+extern bool smaller(int a, int b);
+extern bool smaller_equal(int a, int b);
 
-inline bool greater(int a, int b)
+bool is_sorted(int size, int *array, bool (*compare_function)(int, int))
 {
-    return a>b;
-}
-inline bool greater_equal(int a, int b)
-{
-    return a>=b;
-}
-inline bool smaller(int a, int b)
-{
-    return a<b;
-}
-inline bool smaller_equal(int a, int b)
-{
-    return a<=b;
-}
-
-
-bool is_sorted(int size, int* array, bool (*compare_function)(int, int ))
-{
-    if(size<2)
+    if (size < 2)
     {
         return true;
     }
-    for(int* i=array+1;i<array+size;i++)
+    for (int *i = array + 1; i < array + size; i++)
     {
-        if(!(*compare_function)(*(i-1),*i))
+        if (!(*compare_function)(*(i - 1), *i))
         {
             return false;
         }
@@ -78,15 +63,7 @@ bool is_sorted(int size, int* array, bool (*compare_function)(int, int ))
     return true;
 }
 
-inline void swap(int* a, int* b)
-{
-    int c = *a;
-    *a = *b;
-    *b = c;
-}
+extern void swap(int *a, int *b);
 
 // if a<=x<=b
-inline bool in_range(int x, int a, int b)
-{
-    return x>=a && x<=b;
-}
+extern bool in_range(int x, int a, int b);
